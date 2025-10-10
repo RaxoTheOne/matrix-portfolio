73b90004bda82a4847fe2e1255d800c68d5fe26d
RaxoTheOne <traxon.bg@gmail.com>
2025-10-10 11:26:39 +0200

    Add BasicAuth middleware alias to application configuration for enhanced authentication handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'basic.auth' => \App\Http\Middleware\BasicAuth::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
